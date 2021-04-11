a086b92d12143abaf0d5213130c19d5967781f3b
Anupam Kataki <anupamkataki@Anupams-iMac.attlocal.net>
2021-04-11 12:37:36 -0500

    ai

--- script.js
+++ script.js
@@ -48,12 +48,17 @@ function cellClick() {
     currentArray[row][col] = 1
     checkWin(1)
     this.setAttribute('class', 'alive');
+    var com = comMove([row, col]);
+    console.log(com)
+    currentArray[com[0]][com[1]] = 2;
+    document.getElementById(com[0] + "_" + com[1]).setAttribute('class', 'alive2');
+    checkWin(2);
   } else {
     currentArray[row][col] = 2
     checkWin(2)
     this.setAttribute('class', 'alive2');
   }
-  count += 1
+  count += 2
   console.log(currentArray)
 }
 
@@ -61,26 +66,116 @@ function checkWin(player) {
   msg = "Player " + player + " Won!"
   if (currentArray[0][0] == player && currentArray[1][0] == player && currentArray[2][0] == player) {
     alert(msg)
+    window.location.reload()
   }
   if (currentArray[0][1] == player && currentArray[1][1] == player && currentArray[2][1] == player) {
     alert(msg)
+    window.location.reload()
   }
   if (currentArray[0][2] == player && currentArray[1][2] == player && currentArray[2][2] == player) {
     alert(msg)
+    window.location.reload()
   }
   if (currentArray[0][0] == player && currentArray[0][1] == player && currentArray[0][2] == player) {
     alert(msg)
+    window.location.reload()
   }
   if (currentArray[1][0] == player && currentArray[1][1] == player && currentArray[1][2] == player) {
     alert(msg)
+    window.location.reload()
   }
   if (currentArray[2][0] == player && currentArray[2][1] == player && currentArray[2][2] == player) {
     alert(msg)
+    window.location.reload()
   }
   if (currentArray[0][0] == player && currentArray[1][1] == player && currentArray[2][2] == player) {
     alert(msg)
+    window.location.reload()
   }
   if (currentArray[0][2] == player && currentArray[1][1] == player && currentArray[2][0] == player) {
     alert(msg)
+    window.location.reload()
   }
+}
+
+function randomCell() {
+  var x = Math.floor(Math.random() * 3);
+  var y = Math.floor(Math.random() * 3);
+  while ((currentArray[x][y]) != 0) {
+    var x = Math.floor(Math.random() * 3);
+    var y = Math.floor(Math.random() * 3);
+  }
+  return [x, y];
+}
+
+function countNeighbors(r, c) {
+  var count = 0
+  if (r != 0) {
+    if (c != 0 && currentArray[r - 1][c - 1] == 1) {
+      return [r - 1, c - 1]
+    }
+    if (currentArray[r - 1][c] == 1) {
+      return [r - 1, c]
+    }
+    if (c != cols - 1 && currentArray[r - 1][c + 1] == 1) {
+      return [r - 1, c + 1]
+    }
+  }
+  if (c != 0 && currentArray[r][c - 1] == 1) {
+    return [r, c - 1]
+  }
+  if (c != cols - 1 && currentArray[r][c + 1] == 1) {
+    return [r, c + 1]
+  }
+  if (r != rows - 1) {
+    if (c != 0 && currentArray[r + 1][c - 1] == 1) {
+      return [r + 1, c - 1]
+    }
+    if (currentArray[r + 1][c] == 1) {
+      return [r + 1, c]
+    }
+    if (c != cols - 1 && currentArray[r + 1][c + 1] == 1) {
+      return [r + 1, c + 1]
+    }
+  }
+  return false
+}
+
+function getMissing(cell1, cell2) {
+  if (cell1[0] == cell2[0]) {
+
+    if (cell1[1] == 0 || cell1[1] == 1 && cell2[1] == 1 || cell2[1] == 0) {
+      return [cell1[0], 2]
+    }
+    if (cell1[1] == 0 || cell1[1] == 2 && cell2[1] == 2 || cell2[1] == 0) {
+      return [cell1[0], 1]
+    }
+    if (cell1[1] == 1 || cell1[1] == 2 && cell2[1] == 2 || cell2[1] == 1) {
+      return [cell1[0], 0]
+    }
+  }
+  if (cell1[1] == cell2[1]) {
+
+    if (cell1[0] == 0 || cell1[0] == 1 && cell2[0] == 1 || cell2[0] == 0) {
+      return [2, cell1[1]]
+    }
+    if (cell1[0] == 0 || cell1[0] == 2 && cell2[0] == 2 || cell2[0] == 0) {
+      return [1, cell1[1]]
+    }
+    if (cell1[0] == 1 || cell1[0] == 2 && cell2[0] == 2 || cell2[0] == 1) {
+      return [0, cell1[1]]
+    }
+  }
+  return false
+}
+
+function comMove(p1) {
+  var neighbor = countNeighbors(p1[0], p1[1])
+  if (neighbor != false) {
+    var missing = getMissing(p1, neighbor)
+    if (missing != false) {
+      return missing
+    }
+  }
+  return randomCell()
 }
